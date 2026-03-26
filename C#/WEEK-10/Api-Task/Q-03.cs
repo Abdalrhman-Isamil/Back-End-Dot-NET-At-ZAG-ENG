3ef56bbe70562578ab5c226bdd1356d54a627b4b
A.Ismail Fathy <abdalrhmanismailfathiy@gmail.com>
2026-03-26 22:52:31 +0200

    Update Q-03.cs

--- C#/WEEK-10/Api-Task/Q-03.cs
+++ C#/WEEK-10/Api-Task/Q-03.cs
@@ -1,13 +1,14 @@
 // Question 3 - Constructor Injection
 // What happens when a request is received?
 
+// When a request is received:
 // 1. The Controller instance is created
-// 2. ASP.NET Core sees that ProductController needs ProductService
+// 2. ASP.NET Core detects that ProductController needs ProductService
 // 3. It creates ProductService
-// 4. ProductService needs IRepository
-// 5. The DI container injects SqlRepository (registered as Scoped)
-// 6. Everything is wired automatically
+// 4. ProductService requires IRepository
+// 5. The DI Container injects SqlRepository (registered as AddScoped)
+// 6. Everything is wired together automatically
 
-// Result
+// Result:
 // ProductService uses SqlRepository without creating it manually
-//  Loose Coupling + cleaner code + easier testing
+//  Loose Coupling + cleaner code + easier maintenance and testing
