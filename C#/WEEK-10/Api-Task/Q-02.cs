f82c33c2e784cf519dc5918e05f613a94663adce
A.Ismail Fathy <abdalrhmanismailfathiy@gmail.com>
2026-03-27 18:25:41 +0200

    Update Q-02.cs

--- C#/WEEK-10/Api-Task/Q-02.cs
+++ C#/WEEK-10/Api-Task/Q-02.cs
@@ -1,15 +1,15 @@
 // Question 2 - Tight Coupling Problem
-// What's the difference between A and B? Which one is better?
+// What's the difference between A and B ? Which one is better ?
 
-// Scenario A:
+// Scenario A :
 // Direct dependency (Tight Coupling)
 // UserService creates EmailService using 'new'
-// Problem: hard to change or test
+// Problem : hard to change or test
 
-// Scenario B:
+// Scenario B :
 // Dependency Injection + Interface
 // UserService receives IEmailService from outside
 // Advantage: flexible + easy to change + easy to test
 
-// Which is better?
+// Which is better ?
 // Scenario B because it uses Loose Coupling and better design
