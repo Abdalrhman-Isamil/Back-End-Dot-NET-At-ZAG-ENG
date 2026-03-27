81ac60ba9af0ca3685efb565804596b8b86060f0
A.Ismail Fathy <abdalrhmanismailfathiy@gmail.com>
2026-03-27 18:26:54 +0200

    Update Q-04.cs

--- C#/WEEK-10/Api-Task/Q-04.cs
+++ C#/WEEK-10/Api-Task/Q-04.cs
@@ -1,14 +1,14 @@
 // Question 4 - DI Container Registration
-// What is the output for each registration?
+// What is the output for each registration ?
 
-// Registration A: Transient
+// Registration A : Transient
 // New instance every time it's requested
 // email1 != email2 → AreSameInstance = false
 
-// Registration B: Scoped
+// Registration B : Scoped
 // Same instance within the same request
 // email1 == email2 → AreSameInstance = true
 
-// Registration C: Singleton
+// Registration C : Singleton
 // One instance for the entire application lifetime
 // email1 == email2 → AreSameInstance = true
