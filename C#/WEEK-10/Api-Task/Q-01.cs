3fe6d880ac4b9464a6782cbbb4787b45325d3a1c
A.Ismail Fathy <abdalrhmanismailfathiy@gmail.com>
2026-03-27 18:24:49 +0200

    Update Q-01.cs

--- C#/WEEK-10/Api-Task/Q-01.cs
+++ C#/WEEK-10/Api-Task/Q-01.cs
@@ -1,15 +1,15 @@
 // What is Dependency?
-// Dependency: when a class relies on another class to work.
+// Dependency when a class relies on another class to work.
 
-// Example:
-// OrderService depends on:
+// Example  :
+// OrderService depends on :
 // - SqlConnection (for database)
 // - EmailSender (for sending emails)
 // - FileLogger (for logging)
 
-// Problem:
+// Problem :
 // The class is directly tied to these implementations (Tight Coupling)
-// → Hard to change or test
+//  Hard to change or test
 
-// Better approach:
+// Better approach :
 // Use Dependency Injection to decouple them and make the code more flexible
